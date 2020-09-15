ac80e639598327cdcbb37376e5e964eac574e068
rupak.salekin <rupaksalekin2018@gmail.com>
2020-09-15 21:11:07 +0100

    final commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import math
+
+
+def binary_search(arr, target):
+    left = 0
+    right = len(arr)-1
+    while(left <= right):
+        mid = math.floor((left+right)/2)
+        if(arr[mid] == target):
+            return mid
+        elif(arr[mid] < target):
+            left = mid + 1
+        elif(arr[mid] > target):
+            right = mid - 1
+    return -1
+
+
+arr = [1, 2, 3, 4, 5, 6]
+
+target = 3
+
+result = binary_search(arr, target)
+
+if result != -1:
+    print('found, the index is', result)
+else:
+    print('not found')
